0d8ceb1cbe033cad323f7eea4535ef88d0ad667b
Meena <somendrameena03@gmail.com>
2019-06-24 13:42:46 +0530

    config changes

--- budget/urls.py
+++ budget/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('', views.home),
-    path('contact', views.contact),
-    path('profile',views.profile),
-    path('addnew',views.AddItemView.as_view()),
-    path('viewall',views.ListItemView.as_view()),
+    path('contact/', views.contact),
+    path('profile/',views.profile),
+    path('addnew/',views.AddItemView.as_view()),
+    path('viewall/',views.ListItemView.as_view()),
     url(r'^view/(?P<pk>\w+)/$',views.DetailItemView.as_view()),
     url(r'^view/(?P<pk>\w+)/edit/$',views.UpdateItemView.as_view()),
     url(r'^view/(?P<pk>\w+)/delete/$',views.DeleteItemView.as_view()),
