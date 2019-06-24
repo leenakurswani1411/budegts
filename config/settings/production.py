96fd234acf2f2f188c913248bc108c5b027b1a98
Meena <somendrameena03@gmail.com>
2019-06-24 13:48:43 +0530

    added heroku host

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,4 +2,4 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['budget-exackt.herokuapp.com','budget-leenakurswani.herokuapp.com']
+ALLOWED_HOSTS = ['budgets-exackt.herokuapp.com', 'budget-leenakurswani.herokuapp.com']
